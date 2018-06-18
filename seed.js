b22e752cfcc29dd23774d33350f45df9c5867c7e
Amy De Genaro <amy.degenaro@gmail.com>
2018-06-18 16:45:31 -0400

    synced database

---
+++ seed.js
@@ -0,0 +1,13 @@
+const db = require('./models');
+
+
+db.sync({force: true})
+  .then(() => {
+    console.log('Database synced!')
+    db.close();
+  })
+  .catch(err => {
+    console.log('Something went wrong!')
+    console.log(err)
+    db.close();
+  })
